dc1571e1627cc9666401be00a49642b9791df4ea
munjh61 <junho9661@gmail.com>
2025-01-22 00:09:32 +0900

    [250122][BOJ]8958

---
+++ MJH/[BOJ]8958/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		String[] ary = new String[t];
+		for(int i = 0 ; i < t ; i++) {
+			ary[i] = sc.nextLine();
+		}
+		sc.close();
+		
+		for(int i = 0 ; i < t ; i++) {
+			int l = ary[i].length();
+			int[] score = new int [l];
+			
+			
+		}
+		
+	}
+}
