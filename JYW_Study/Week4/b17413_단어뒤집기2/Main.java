2ecdd73f09dd50d9a659b14a936bddae51fe9b66
yeonwju <yeonwju@gmail.com>
2025-02-20 17:39:28 +0900

    단어 거꾸로 아직 못함

---
+++ JYW_Study/Week4/b17413_단어뒤집기2/Main.java
@@ -0,0 +1,24 @@
+package b17413_단어뒤집기2;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char[] line = sc.next().toCharArray();
+		
+		char[] nline = new char[line.length];
+		
+		for(int i=0; i<line.length; i++) {
+			if(line[i] == 0) {
+				int index = i;
+				
+				for(int)
+				
+				
+			}
+		}
+		
+	}
+}
