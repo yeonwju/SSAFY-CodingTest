bffa7cf1bc003da36cac3f853c65e288ff92b601
yeonwju <yeonwju@gmail.com>
2025-03-06 16:28:26 +0900

    비밀이메일 수정

--- JYW_Study/Week6/b2999_비밀이메일/Main.java
+++ JYW_Study/Week6/b2999_비밀이메일/Main.java
@@ -15,49 +15,36 @@ public class Main {
 		int r;
 		int c;
 		
-		if(N%2 == 1) {//홀수일 경우
-			r = N;//행의 갯수가 N개
-			c = 1;
-			ans = new char[r][c];
+		int max = 0;
+		for(int i=1; i<=Math.sqrt(N); i++) {// Math.sqrt 제곱근해서 거기까지 계산하기
+			int value = N%i;
 			
-			for(int i=0; i<r; i++) {
-				for(int j=0; j<c; j++) {
-					ans[i][j] = str.charAt(i);
-				}
-			}
-			
-		} else {
-			
-			int max = 0;
-			for(int i=1; i<=Math.sqrt(N); i++) {// Math.sqrt 제곱근해서 거기까지 계산하기
-				int value = N%i;
+			if(value == 0) {//N이 i로 떨어지는 값일 경우
+				int p = i; //i값을 p에 저장
 				
-				if(value == 0) {//N이 i로 떨어지는 값일 경우
-					int p = i; //i값을 p에 저장
-					
-					if(max<p) {
-						max = p; //max값 갱신
-					}
+				if(max<p) {
+					max = p; //max값 갱신
 				}
 			}
-			
-			r = max;//r값이 c보다 더 크니까
-			c = N/max;//r*c=N이니까
-			
-			ans = new char[r][c]; // 배열 크기 설정해서 선언
-			
-			int k=0;
-			for(int i=0; i<r; i++) {
-				for(int j=0; j<c; j++) {
-					ans[i][j] = str.charAt(k);
-					k++;
-				}
-			}
-			
 		}
 		
-		for(int j=0; j<c; j++) {
+		r = max;//r값이 c보다 더 크니까
+		c = N/max;//r*c=N이니까
+		
+//		System.out.println(r+" "+c);
+		
+		ans = new char[r][c]; // 배열 크기 설정해서 선언
+		
+		int k=0;
+		for(int j=0; j<c; j++) {//열고정 세로 입력
 			for(int i=0; i<r; i++) {
+				ans[i][j] = str.charAt(k);
+				k++;
+			}
+		}
+		
+		for(int i=0; i<r; i++) {//행고정 가로 출력
+			for(int j=0; j<c; j++) {
 				System.out.print(ans[i][j]);
 			}
 		}
