1b3d51d65637cb04c777f75a7d397df6a64eabfd
yeonwju <yeonwju@gmail.com>
2025-03-11 16:49:07 +0900

    숫자카드2

--- JYW_Study/Week7/b10816_숫자카드2/Main.java
+++ JYW_Study/Week7/b10816_숫자카드2/Main.java
@@ -1,41 +1,47 @@
 package b10816_숫자카드2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		Map<Integer, Integer> hashMap = new HashMap<>();
-		
-		int N = sc.nextInt();
-		int[] sang = new int[N];
-		
-		int M = sc.nextInt();
-		int[] sol = new int[M];
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Map<Integer, Integer> map = new HashMap<>();
 		
 		for(int i=0; i<N; i++) {
-			sang[i] = sc.nextInt();
-		}
-		
-		for(int i=0; i<M; i++) {
-			sol[i] = sc.nextInt();
-		}
-		
-		int max=0;
-		for(int i=0; i<N; i++) {
-			if(sang[i]>max) {
-				max = sang[i];
+			int num = Integer.parseInt(st.nextToken());
+			if(map.containsKey(num)) {
+				int a = map.get(num);
+				a++;
+				map.put(num, a);
+			} else {
+				map.put(num, 1);
 			}
 		}
 		
 		
+		int M = Integer.parseInt(br.readLine());
 		
-		
-		
-		
-		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<M; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(map.containsKey(a)) {
+				bw.write(map.get(a)+" ");
+			}else {
+				bw.write(0+" ");
+			}
+		}
+		bw.flush();
 	}
 }
