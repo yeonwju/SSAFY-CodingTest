47ddb0fc916e196dbea60e822569b67744e9be98
junho <junho9661@gmail.com>
2025-06-13 09:52:10 +0900

    0613

--- MJH/2506/BOJ_2178/src/Main.java
+++ MJH/2506/BOJ_2178/src/Main.java
@@ -25,46 +25,46 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = br.readLine().split(" ");
-		N = Integer.parseInt(line[0]);
-		M = Integer.parseInt(line[1]);
+		String[] nm = br.readLine().split(" ");
+		N = Integer.parseInt(nm[0]);
+		M = Integer.parseInt(nm[1]);
 		matrix = new int[N][M];
 		visited = new boolean[N][M];
 		for (int row = 0; row < N; row++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 0 ; i < M; i ++) {
-				matrix[row][i] = Integer.parseInt(st.nextToken());
+			String l = br.readLine();
+			for (int i = 0; i < M; i++) {
+				matrix[row][i] = l.charAt(i) - '0';
 			}
 		}
 		// bfs
+		int move = 1;
 		Queue<Loc> q = new LinkedList<>();
 		q.add(new Loc(0, 0));
 		visited[0][0] = true;
-		int move = 0;
-		boolean flag = false;
-		while (!q.isEmpty()) {
-			Loc curr = q.poll();
-			for (int dir = 0; dir < 4; dir++) {
-                int nx = curr.x + dx[dir];
-                int ny = curr.y + dy[dir];
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-                    continue;
-                if (matrix[nx][ny] == 0 || visited[nx][ny])
-                    continue;
-                visited[nx][ny] = true;
-                q.add(new Loc(nx, ny));
-                if(nx == N -1 && ny == M -1) {
-                	flag = true;
-                	move++;
-                	break;
-                }
-			}
-			move++;
-		}
-		// 이 문제에서는 못 도착하는 경우가 없게 내나보다
-		if(flag) {
-			System.out.println(move);
+		while (!q.isEmpty()) {
+		    int size = q.size(); // 이번 움직임 안에 갈 수 있는 경로
+		    for (int i = 0; i < size; i++) {
+		        Loc curr = q.poll();
+		        if (curr.x == N - 1 && curr.y == M - 1) {
+		            System.out.println(move);
+		            return;
+		        }
+
+		        for (int dir = 0; dir < 4; dir++) {
+		            int nx = curr.x + dx[dir];
+		            int ny = curr.y + dy[dir];
+
+		            if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+		                continue;
+		            if (matrix[nx][ny] == 0 || visited[nx][ny])
+		                continue;
+
+		            visited[nx][ny] = true;
+		            q.add(new Loc(nx, ny));
+		        }
+		    }
+		    move++;
 		}
 	}
 }
